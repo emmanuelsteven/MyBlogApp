e3ad3d7d4bc35d6876ea6fcf89cf74bef3908253
emmanuelsteven <write2stevemmy@gmail.com>
2023-08-19 09:39:29 +0100

    model-data-processing

---
+++ app/models/comment.rb
@@ -0,0 +1,19 @@
+class Comment < ApplicationRecord
+  # associations
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+
+  # Attributes
+  attribute :text, :text
+
+  # callbacks
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  # methods
+  private
+
+  def update_post_comments_counter
+    post.update(comment_counter: post.comments.count)
+  end
+end
