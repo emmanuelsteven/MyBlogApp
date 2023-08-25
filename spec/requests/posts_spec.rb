5a6a4cfaee9ce7ea375bac117c00adabb0e1e831
emmanuelsteven <write2stevemmy@gmail.com>
2023-08-25 08:18:33 +0100

    update spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe 'Posts', type: :request do
     it 'renders the show template' do
       user = User.create(name: 'steven', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'series from jos',
                          post_counter: 0)
-      post = Post.create(title: 'post1', author: user, likes_counter: 0, comment_counter: 0, created_at: 5.day.ago)
-    get user_post_path(:user_id, :post_id)
+      Post.create(title: 'post1', author: user, likes_counter: 0, comment_counter: 0, created_at: 5.day.ago)
+      get user_post_path(:user_id, :post_id)
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
       expect(response.body).to include('Details of a selected post')
